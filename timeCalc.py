e4809d760cacfaadd912ac618efd6b7eb271b82c
Matthew Hughes <mhughes@rtautomation.com>
2020-03-17 11:35:38 -0500

    adding help

--- timeCalc.py
+++ timeCalc.py
@@ -1,13 +1,27 @@
 #!/usr/bin/python3
 from datetime import datetime
 import os, sys
-s1 = sys.argv[1]
-s2 = sys.argv[2]
 
-FMT = '%H:%M'
 
-tdelta = datetime.strptime(s2, FMT) - datetime.strptime(s1, FMT)
+def printHelp():
+    print("""timeCalc.py <start_time> <end_time>""")
+    pass
 
-hours = tdelta.total_seconds() / 3600.
 
-print(tdelta, hours)
+def calc(s1, s2):
+    tdelta = datetime.strptime(s2, FMT) - datetime.strptime(s1, FMT)
+    hours = tdelta.total_seconds() / 3600.
+    return tdelta, hours
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3 :
+        printHelp()
+        exit(1)
+
+    s1 = sys.argv[1]
+    s2 = sys.argv[2]
+
+    FMT = '%H:%M'
+    tdelta, hours = calc(s1, s2)
+
+    print(tdelta, hours)
