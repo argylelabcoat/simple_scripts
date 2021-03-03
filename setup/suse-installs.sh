cb41d6aaa424cf0e766303878e8d09a771f60a90
Matthew <matt@argylelabco.at>
2021-03-03 16:58:02 -0600

    packages for my preferred opensuse setup

---
+++ setup/suse-installs.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+#################################################
+## Source Control
+
+sudo zypper install git mercurial subversion fossil
+
+#################################################
+## Development Patterns
+
+sudo zypper in -t pattern devel_basis devel_C_C++ devel_python
+
+#################################################
+## Development Libs / Tools
+
+sudo zypper install  openssl-devel clang-tools \
+lazarus anjuta anjuta-extras glade codelite gnome-builder \
+neovim emacs \
+scons ninja direnv
+
+#################################################
+## Containers / Tools
+
+sudo zypper install podman buildah
+
+
+#################################################
+## BC-4
+
+sudo rpm --import https://www.scootersoftware.com/RPM-GPG-KEY-scootersoftware
+sudo zypper refresh
+sudo zypper install https://www.scootersoftware.com/bcompare-4.3.7.25118.x86_64.rpm
+
+
+#################################################
+## Sublime Tools
+
+sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
+sudo zypper addrepo -g -f https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
+sudo zypper install sublime-text sublime-merge
+
