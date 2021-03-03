b0dbb13483352fc0a7b75ee348bb9b51dd0b85df
Matthew <matt@argylelabco.at>
2021-03-03 15:27:59 -0600

    updating install starship to also install rust if needed.

--- install_starship.sh
+++ install_starship.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
+
+source $HOME/.cargo/env
+if ! command -v cargo %> /dev/null
+then
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+  source $HOME/.cargo/env
+fi
+
 cargo install starship
