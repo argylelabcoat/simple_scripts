27746a0e3ad40a0f1ae56b2cc3564d2e751526ce
Matthew Hughes <matt@argylelabco.at>
2021-05-22 16:45:00 -0500

    improve asdf + direnv + poetry experience

--- setup/install_asdf.sh
+++ setup/install_asdf.sh
@@ -20,7 +20,21 @@ asdf global  direnv 2.28.0
 mkdir -p $HOME/.config/direnv
 
 cat >> $HOME/.config/direnv/direnvrc <<'EOF'
- source "$(asdf direnv hook asdf)" 
+source "$(asdf direnv hook asdf)" 
+
+layout_poetry() {
+  if [[ ! -f pyproject.toml ]]; then
+    log_error 'No pyproject.toml found. Use `poetry new` or `poetry init` to create one first.'
+    exit 2
+  fi
+
+  # create venv if it doesn't exist
+  poetry run true
+
+  export VIRTUAL_ENV=$(poetry env info --path)
+  export POETRY_ACTIVE=1
+  PATH_add "$VIRTUAL_ENV/bin"
+}
 EOF
 
 cat >> $HOME/.tool-versions <<'EOF'
